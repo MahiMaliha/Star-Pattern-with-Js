b7b36febb842363dc3aeb4f7a88664eb9036e9b8
Jannatul Mahiya Mutasha <87617184+JannatulMutashaEngineer@users.noreply.github.com>
2021-09-09 03:17:40 +0600

    Add files via upload

---
+++ array-loop.js
@@ -0,0 +1,21 @@
+// Run Loop in Array
+
+// Using For loop
+let names = ['Akash', 'Batash', 'Hotash'];
+
+console.log('Using For Loop:');
+for (var i = 0; i < names.length; i++) {
+        var name = names[i];
+        console.log(name);
+}
+
+
+// Using While Loop
+
+console.log('Using While Loop:');
+var i = 0;
+while (i < names.length) {
+        var name = names[i];
+        i++;
+        console.log(name);
+}
---
+++ odd-even-number.js
@@ -0,0 +1,14 @@
+// Odd Number
+
+for (var i = 1; i <= 50; i += 2) {
+        console.log(i);
+}
+
+// Even Number
+
+for (var i = 0; i <= 50; i += 2) {
+        console.log(i);
+}
+
+var name = 'false';
+console.log(typeof name);
